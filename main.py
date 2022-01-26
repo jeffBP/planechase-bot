da3ac94c7c87ccf1ce2bd08e6fba54aafd9ab227
Jeff Pflueger <jeffpflueger1@gmail.com>
2022-01-26 15:25:18 -0500

    create planechase logic and bot

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import discord
 import os
+import time
 
-from .planechase import planechase
+import planechase as pc
 
 client = discord.Client()
+
+game = pc.Planechase()
 
 @client.event
 async def on_ready():
@@ -15,13 +18,48 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
     if message.content.startswith('/startgame'):
-        await message.channel.send()
+        content_str = "Welcome to planechase!"
+        await message.channel.send(content_str)
 
+    rpd_outcome = None
     if message.content.startswith('/rpd'):
-        await message.channel.send(s)
+        content_str="Rolling planar die..."
+        await message.channel.send(content_str)
+        rpd_outcome = game.roll_planar_die()
+        if (rpd_outcome==""):
+            content_str = "Nothing Happens."
+            await message.channel.send(content_str)
+        elif (rpd_outcome=="chaos"):
+            content_str = "Chaos rolled!"
+            await message.channel.send(content_str)
+        elif (rpd_outcome=="planeswalk"):
+            content_str = "Planeswalk rolled!"
+            await message.channel.send(content_str)
+    
+    if message.content.startswith('/plane') or message.content.startswith('/startgame') or rpd_outcome == "planeswalk": 
+        pic_file = discord.File(fp=game.get_current_plane_image())
+        content_str = "**Current Plane**: {}\n\n".format(game.get_current_plane_name())
+        print(len(pic_file))
+        # await message.channel.send(content_str, file=pic_embed)
+        time.sleep(0.3)
 
-client.run(os.env('TOKEN'))
+    if message.content.startswith('/static') or message.content.startswith('/startgame') or message.content.startswith('/plane'):
+        content_str = "**Static ability**: {}".format(game.get_current_plane_static_ability())
+        await message.channel.send(content_str)
+
+    if message.content.startswith('/chaos') or message.content.startswith('/startgame') or message.content.startswith('/plane') or rpd_outcome == "chaos":
+        content_str = "**Chaos ability**: {}".format(game.get_current_plane_chaos_ability() if game.get_current_plane_chaos_ability() else "None")
+        await message.channel.send(content_str)
+    
+    if message.content.startswith('/help'):
+        content_str = "**Bot commands:**\n"
+        content_str += "**/start_game:** Start a new game and walks to a random plane.\n"
+        content_str += "**/rpd:** Roll the planar die.\n"
+        content_str += "**/plane:** Get information about the current plane.\n"
+        content_str += "**/static:** Get the current plane's static ability.\n"
+        content_str += "**/chaos:** Get the current plane's chaos ability if it has one."
+        await message.channel.send(content_str)
+
+client.run(os.getenv('TOKEN'))
+    
--- planechase.py
+++ planechase.py
@@ -1,5 +1,110 @@
+import requests
 import json
+import time
+import random
+from PIL import Image
+from io import BytesIO
+
+PLANECHASE_REQUEST = "http://api.scryfall.com/sets/opca?as=grid&order=set"
+
+PLANAR_DIE_OUTCOMES = ["", "chaos", "planeswalk"]
+PLANAR_DIE_WEIGHTS = [4, 1, 1]
+    
+def get_pc_set():
+    pc_set = requests.get(PLANECHASE_REQUEST)
+    time.sleep(0.3)
+    return pc_set.json()
+
+def get_pc_cards():
+    pc_set_json = get_pc_set()
+    pc_cards = requests.get(pc_set_json['search_uri'])
+    time.sleep(0.3)
+    return pc_cards.json()
 
 class Planechase(object):
-    def __init__():
+    def __init__(self):
         self._name = "planechase"
+        self._cards_json = get_pc_cards()
+        self._planes = self._cards_json['data']
+        self._num_cards = self._cards_json['total_cards']
+        print("Loaded planechase cards!")
+        self._current_plane = Plane()
+        self._planeswalk()
+
+    def roll_planar_die(self):
+        choice = random.choices(PLANAR_DIE_OUTCOMES, PLANAR_DIE_WEIGHTS)[0]
+        if (choice == "chaos"):
+            self._chaos()
+        elif (choice == "planeswalk"):
+            self._planeswalk()
+        return choice
+    
+    def _planeswalk(self):
+        plane_data = random.choice(self._planes)
+        if (self._current_plane.get_name() == plane_data['name']) :
+            self._planeswalk()
+            return
+        self._current_plane = Plane(plane_data['name'], plane_data['oracle_text'], plane_data['image_uris']['small'])
+    
+    def _chaos(self):
+        print(self._current_plane.get_chaos_ability())
+     
+    def get_current_plane_name(self):
+        return self._current_plane.get_name()
+
+    def get_current_plane_ability(self):
+        return self._current_plane.get_ability()
+    
+    def get_current_plane_static_ability(self):
+        return self._current_plane.get_static_ability()
+
+    def get_current_plane_chaos_ability(self):
+        return self._current_plane.get_chaos_ability()
+
+    def get_current_plane_image(self):
+        return self._current_plane.get_plane_image()
+
+
+class Plane(object):
+    def __init__(self, name="", ability="", image_uri=""):
+        self._name = name
+        self._ability = ability
+        self._image_uri = image_uri
+        self._static_ability = ""
+        self._chaos_ability = ""
+        self._parse_ability(self._ability)
+        self._image = None
+
+    def _parse_ability(self, ability):
+        ability_split = ability.split('Whenever you roll {CHAOS}, ')
+        self._static_ability = ability_split[0]
+        if (len(ability_split) == 1):
+            return
+        else:
+            self._chaos_ability = ability_split[1]
+        self._chaos_ability = self._chaos_ability[0].upper() + self._chaos_ability[1:]
+
+    def get_name(self):
+        return self._name
+
+    def get_ability(self):
+        return self._ability
+
+    def get_static_ability(self):
+        return self._static_ability
+
+    def get_chaos_ability(self):
+        return self._chaos_ability
+
+    def _load_plane_image(self):
+        print("Loading image")
+        image_response = requests.get(self._image_uri)
+        plane_img = Image.open(BytesIO(image_response.content))
+        plane_img.rotate(-90)
+        self._image = plane_img.tobytes()
+        time.sleep(0.3)
+
+    def get_plane_image(self):
+        if (not self._image):
+            self._load_plane_image()
+        return self._image
